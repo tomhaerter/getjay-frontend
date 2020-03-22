dc77f2ad3d0b8f1a59a026a2ca3a075b17ab7d68
owlnights <tom@dokedu.org>
2020-03-23 00:21:38 +0100

    Use category provided by the backend

--- components/Offers/EmployerBanner.vue
+++ components/Offers/EmployerBanner.vue
@@ -4,11 +4,11 @@
       <img src="~/assets/bauer-logo.png" />
     </div>
     <div class="ml-4">
-      <h3 class="name font-semibold text-l leading-narrow">Bauernhof Wieland</h3>
+      <h3 class="name font-semibold text-l leading-narrow">{{offer.title}}</h3>
       <p class="text-xs">Dorfstraße Nr. 14, 23769 Fehmarn</p>
 
       <div class="categories mt-2">
-        <span class="category text-xs text-white bg-petrol rounded-md uppercase py-1 px-2">Landwirtschaft</span>
+        <span class="category text-xs text-white bg-petrol rounded-md uppercase py-1 px-2">{{category}}</span>
       </div>
     </div>
   </div>
@@ -23,6 +23,24 @@ export default Vue.extend({
       type: Object,
       required: true,
     },
+  },
+  mounted() {
+    console.log(this.offer);
+  },
+  computed: {
+    category() {
+      if (this.offer.categories.length > 0) {
+        switch (this.offer.categories[0]) {
+          case "1": return 'Landwirtschaft'
+          case "2": return 'Medizin'
+          case "3": return 'Pflege'
+          case "4": return 'Logistik'
+          default: return 'Sonstiges'
+        }
+      }
+
+      return 'Sonstiges'
+    }
   },
 })
 </script>
