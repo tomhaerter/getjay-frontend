2f64aa3ef16b608aa0f3a0197cad6a90cb60e995
Christopher Mühl <christopher@padarom.xyz>
2020-03-22 16:33:11 +0100

    Use reusable apple sheet component for login

--- layouts/default.vue
+++ layouts/default.vue
@@ -99,7 +99,7 @@
       </main>
     </div>
 
-    <login-card :open="openLogin" @close="openLogin = false" />
+    <login-sheet :open="openLogin" @close="openLogin = false" />
 
     <div v-if="!user" class="fixed inset-x-0 cursor-pointer" style="bottom: 68px;" @click="openLogin = true">
       <div class="bg-pink text-petrol">
@@ -166,11 +166,11 @@
 <script lang="ts">
 import Vue from 'vue'
 import mobile from 'is-mobile'
-import LoginCard from '~/components/LoginCard.vue'
+import LoginSheet from '~/components/LoginSheet.vue'
 
 export default Vue.extend({
   components: {
-    LoginCard,
+    LoginSheet,
   },
 
   data () {
